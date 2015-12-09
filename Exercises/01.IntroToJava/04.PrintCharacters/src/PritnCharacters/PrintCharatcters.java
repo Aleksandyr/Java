e25cdabac78d1040fd9d21251a4642bc1b934000
Aleksandyr <aleksandyr.kamenov@gmail.com>
2015-12-09 16:23:13 +0200

    Print character task

---
+++ Exercises/01.IntroToJava/04.PrintCharacters/src/PritnCharacters/PrintCharatcters.java
@@ -0,0 +1,12 @@
+package PritnCharacters;
+
+public class PrintCharatcters {
+
+	public static void main(String[] args) {
+		for(char i = 'a'; i <= 'z'; i++){
+			System.out.print(i + " ");
+		}
+
+	}
+
+}
