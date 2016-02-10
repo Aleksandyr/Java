f463a43a76a66d0f41a360ddab157c5788b989ec
Aleksandyr <aleksandyr.kamenov@gmail.com>
2016-02-10 22:30:05 +0200

    excersice for enterprise java beans stateful ready

---
+++ Java-Web-Development-Homeworks/04.EnterpriseJavaBeansExercise/src/bg/jwd/ejb/DateCollectionImpl.java
@@ -0,0 +1,22 @@
+package bg.jwd.ejb;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.Stateful;
+
+@Stateful
+public class DateCollectionImpl implements DateCollection {
+
+	private List<String> dates = new ArrayList<>();
+	
+	@Override
+	public String addAndReturn() {
+		dates.add((new Date()).toString());
+		
+		return Arrays.toString(dates.toArray());
+	}
+
+}
